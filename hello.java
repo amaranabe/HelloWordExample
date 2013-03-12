4646c17706a4a75d8f2ca29063ef6261ef4fd837
yaman-ghazi <yaman-ghazi-1990@hotmail.com>
2013-03-12 22:19:30 +0200

    Create hello.java

---
+++ hello.java
@@ -0,0 +1,8 @@
+public class hello {
+
+    public static void main(String[] args) {
+        System.out.println("My Name is Yaman Ghazi");
+        System.out.println("Hello Word All");
+    }
+
+}
